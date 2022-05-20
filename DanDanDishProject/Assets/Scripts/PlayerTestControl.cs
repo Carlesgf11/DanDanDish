3004810718070b3966ef8244214851e8363f4bf3
SrTrolo <albertpitarque02@gmail.com>
2022-05-20 16:30:51 +0200

    Desmembramiento

---
+++ DanDanDishProject/Assets/Scripts/PlayerTestControl.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTestControl : MonoBehaviour
+{
+    public GameObject ps, blood, bodyParts, bodySprite;
+    public float fieldOfImpact, force;
+    public LayerMask layerToHit;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            Die();
+        }
+    }
+    public void Die()
+    {
+        Vector2 pos = new Vector2(transform.position.x, (transform.position.y - (Random.Range(0f, 1f))));
+        Instantiate(blood, pos, Quaternion.identity);
+        Instantiate(ps, transform.position, ps.transform.rotation);
+        bodyParts.SetActive(true);
+        bodySprite.SetActive(false);
+        Collider2D[] objects = Physics2D.OverlapCircleAll(pos, fieldOfImpact, layerToHit);
+        foreach(Collider2D obj in objects)
+        {
+            Vector2 direction = (Vector2)obj.transform.position - pos;
+            obj.GetComponent<Rigidbody2D>().AddForce(direction * force * 100f);
+
+        }
+        bodyParts.transform.SetParent(null);
+        Destroy(gameObject);
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, fieldOfImpact);
+    }
+}
